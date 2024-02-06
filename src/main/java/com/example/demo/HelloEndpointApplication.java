58978edbba87a1cc19c5d723fc21cdb119c23787
Soham Patra <patrasoham1512@gmail.com>
2024-02-06 12:36:55 +0530

    getallemployees

--- src/main/java/com/example/demo/HelloEndpointApplication.java
+++ src/main/java/com/example/demo/HelloEndpointApplication.java
@@ -2,11 +2,16 @@ package com.example.demo;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Employee;
 
 @SpringBootApplication
 @RestController
@@ -19,6 +24,16 @@ public class HelloEndpointApplication {
 	@GetMapping
 	public String status() throws UnknownHostException {
 		return "status-up"+InetAddress.getLocalHost().toString();
+	}
+	
+	@GetMapping("/employees")
+	public List<Employee> getAllEmployees(){
+		List<Employee> list = Stream.of(
+				new Employee("1","soham","soham@email.com"),
+				new Employee("2","anjali","anjali@email.com"),
+				new Employee("3","arundhuti","arundhuti@email.com")
+				).collect(Collectors.toList());
+		return list;
 	}
 
 }
